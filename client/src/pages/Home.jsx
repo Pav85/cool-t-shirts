12f68c1fa023d0e607c4fb68a672f9e933d579af
Pav85 <pawel.werbowy@gmail.com>
2023-07-21 13:16:04 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
             </motion.div>
 
             <motion.div
-              {...headContainerAnimation}
+              {...headContentAnimation}
               className="flex flex-col gap-5"
             >
               <p className="max-w-md font-normal text-gray-600 text-base">
