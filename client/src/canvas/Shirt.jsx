9a440cdc89703f4d3f46a3a618c7e2c12d830cde
Pav85 <pawel.werbowy@gmail.com>
2023-07-21 16:00:33 +0100

    Update Shirt.jsx

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -9,6 +9,9 @@ const Shirt = () => {
   const snap = useSnapshot(state);
   const { nodes, materials } = useGLTF("/shirt_baked.glb");
 
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+
   return <div>Shirt</div>;
 };
 
