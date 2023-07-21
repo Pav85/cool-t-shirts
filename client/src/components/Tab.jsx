5ea7b78ce269e24f70979c64cef2233428325346
Pav85 <pawel.werbowy@gmail.com>
2023-07-21 23:15:03 +0100

    Update Tab.jsx

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useSnapshot } from "valtio";
+import state from "../store";
 
-const Tab = () => {
-  return <div>Tab</div>;
+const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
+  const snap = useSnapshot(state);
+
+  const activeStyles =
+    isActiveTab && isFilterTab
+      ? { backgroundColor: snap.color, opacity: 0.5 }
+      : { backgroundColor: "transparent", opacity: 1 };
+
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn ${
+        isFilterTab ? "rounded-full glassmorhism" : "rounded-4"
+      }`}
+      onClick={handleClick}
+    >
+      Tab
+    </div>
+  );
 };
 
 export default Tab;
