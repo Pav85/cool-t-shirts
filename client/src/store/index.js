6d27de2d07ffdbc1eb3a88d591adf3260b8db538
Pav85 <pawel.werbowy@gmail.com>
2023-07-21 12:20:31 +0100

    Create index.js

---
+++ client/src/store/index.js
@@ -0,0 +1,11 @@
+import { proxy } from "valtio";
+
+const state = prox({
+  intro: true,
+  color: "#EFBD48",
+  isLogoTexture: true,
+  isFullTexture: false,
+  logoDecal: "./threejs.png", // check that later
+});
+
+export default state;
