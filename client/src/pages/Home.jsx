62c24a9400a7da0267d0df31993cc6cecda7b09f
Pav85 <pawel.werbowy@gmail.com>
2023-07-21 12:51:55 +0100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,7 +17,11 @@ const Home = () => {
       {snap.intro && (
         <motion.section className="home" {...slideAnimation("left")}>
           <motion.header>
-            <img src="./threejs.png" />
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
           </motion.header>
         </motion.section>
       )}
