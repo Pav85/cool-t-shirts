3764b34a2f5a8a265b8accdfbad0cb41f55f4a8a
Pav85 <pawel.werbowy@gmail.com>
2023-07-21 22:09:00 +0100

    t-shirt size fix

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -5,8 +5,8 @@ import { AccumulativeShadows, RandomizedLight } from "@react-three/drei";
 
 const Backdrop = () => {
   return (
-    <AccumulativeShadows>
-      <RandomizedLight />
+    <AccumulativeShadows position={[0, 0, 0.14]}>
+      <RandomizedLight amount={4} />
     </AccumulativeShadows>
   );
 };
