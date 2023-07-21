176fd305dfdda88907f4c5921a6ca725d643723a
Pav85 <pawel.werbowy@gmail.com>
2023-07-21 12:02:49 +0100

    Update index.jsx

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const index = () => {
-  return <div>index</div>;
+const Canvas = () => {
+  return <div>Canvas</div>;
 };
 
-export default index;
+export default Canvas;
