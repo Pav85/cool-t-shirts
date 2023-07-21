b03f34abed5d2087ea9ba01bcdf2e98503340625
Pav85 <pawel.werbowy@gmail.com>
2023-07-21 13:34:24 +0100

    Update CustomButton.jsx

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const CustomButton = () => {
-  return <div>CustomButton</div>;
+const CustomButton = ({ title }) => {
+  return <button className={"px-2"}>{title}</button>;
 };
 
 export default CustomButton;
