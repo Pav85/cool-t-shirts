36c626130051983936dfb2e7caa24cc57cbdc8bc
Pav85 <pawel.werbowy@gmail.com>
2023-08-02 15:12:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(express.json({ limit: "50mb" })); // possible error
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E" });
+});
